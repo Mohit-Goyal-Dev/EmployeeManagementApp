229d6cbc85da9fe75b01bc406b2f486ef0d1166f
Mohit Goyal <mr.rishigoyal@gmail.com>
2022-02-10 17:33:06 +0530

    Updated for deployment

--- backend/database/dbConifg.js
+++ backend/database/dbConifg.js
@@ -1,4 +1,4 @@
 module.exports = {
    // db: 'mongodb://localhost:27017/EmployeeDB'
-   db: 'mongodb+srv://<user>:<password>@<host>/EmployeeDB?retryWrites=true&w=majority'
+   db: `mongodb+srv://mohit:${process.env.MONGO_ATLAS_PWD}@cluster0.nie5u.mongodb.net/EmployeeDB?retryWrites=true&w=majority`
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://empmanager-api.herokuapp.com/api",
 };
